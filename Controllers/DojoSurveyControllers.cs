81b7670111c8d80bb8c67c759e0ec37d0de67a57
He Ai <heai0210@gmail.com>
2019-04-05 01:36:28 -0700

    survey

---
+++ Controllers/DojoSurveyControllers.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+namespace Dojo_Survey.Controllers
+{
+
+    public class DojoSurveyController: Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [HttpPost]
+        [Route("result")]
+        public IActionResult result(string name, string location, string language, string comment)   
+        {
+            ViewBag.name = name;
+            ViewBag.location = location;
+            ViewBag.language = language;
+            ViewBag.comment = comment;
+            Console.WriteLine(ViewBag.name + ViewBag.location + ViewBag.language + ViewBag.comment);
+            return View("result");
+        }    
+    }
+
+}
